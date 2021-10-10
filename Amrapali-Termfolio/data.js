1d8a7b4fae3f94450735054906be568499d90ff0
Er. Amrapali Palkar <80786215+Amrapalipalkar@users.noreply.github.com>
2021-10-10 14:01:15 +0530

    Add files via upload

---
+++ Amrapali-Termfolio/data.js
@@ -0,0 +1,39 @@
+var data = {
+    
+    bio: [
+      {name: 'Name', value: 'Amrapali Hiru Palkar'},
+      {name: 'Email', value: 'amrapalipalkar24@gmail.com'},
+      {name: 'Phone', value: '7841834650'},
+      {name: 'Country', value: 'India'},
+      {name: 'City', value: 'Mumbai'}
+    ],
+    github: [
+      {name: 'GitHub', value: 'Amrapalipalkar'}
+    ],
+    
+    skills: [
+
+    ],
+    certificates: [
+
+    ],
+    portfolio: [
+      {name: 'Portfolio', value: 'https://ahsankhan.me'}
+    ],
+    projects: [
+      {name: 'Book Blog', value: 'https://github.com/ahsankhan26/Book-Blog'},
+      {name: 'Bomberman Replica', value: 'https://github.com/ahsankhan26/Bomberman-Replica'},
+      {name: 'WhatsApp Chat Parser', value: 'https://github.com/ahsankhan26/Whatsapp-Chat-Parser'},
+      {name: 'Laser Data Transmission', value: 'https://github.com/ahsankhan26/Laser-Data-Transmission'},
+      {name: 'Color Guessing Game', value: 'https://github.com/ahsankhan26/Color-Guessing-Game'},
+      {name: 'GPA Calculator', value: 'https://github.com/ahsankhan26/gpa-calculator'},
+      {name: 'Hello World but unnecessarily complex', value: 'https://github.com/ahsankhan26/hello-world'}
+    ],
+    resume: 'Ahsan_Khan_CV.pdf',
+    contact: [
+      {name: 'mobile', value: '7841834650'},
+      {name: 'LinkedIn', value: 'https://www.linkedin.com/in/amrapali-palkar-030558170/'},
+      {name: 'GitHub', value: 'https://github.com/Amrapalipalkar'},
+      {name: 'Instagram', value: 'https://instagram/imamrapali'}
+    ]
+  }
