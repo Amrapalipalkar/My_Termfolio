d5ddbc1b1c934cdcd1454f1f49ec2b608bbf94e2
Er. Amrapali Palkar <80786215+Amrapalipalkar@users.noreply.github.com>
2021-10-12 15:54:35 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,41 @@
+var data = {
+    
+  bio: [
+    {name: 'Name', value: 'Amrapali Hiru Palkar'},
+    {name: 'B.Tech', value: 'Information Technology'},
+    // {name: 'Hobby' , value: 'Reading', 'Writting', 'Coding', 'Travelling'},
+    {name: 'Country', value: 'India'},
+    {name: 'City', value: 'Mumbai'}
+  ],
+  github: [
+    {name: 'GitHub', value: 'Amrapalipalkar'}
+  ],
+  
+  skills: [
+      {name: 'Frontend' , value: 'ReactJS HTML CSS Javascript Bootstap'},
+      {name: 'Backend' , value: 'c NodeJS ExpressJS'},
+      {name: 'Database' , value: 'MongoDB'},
+      {name: 'Tools' , value: 'Github  VScode'}
+  ],
+  certificates: [
+
+  ],
+  portfolio: [
+    {name: 'Portfolio', value: ''}
+  ],
+  projects: [
+    {name: 'Mentor Assinging Portal', value: ''},
+    {name: 'MI Team-curd Website', value: ''},
+    {name: 'Live covid-19 Tracker', value: ''},
+    {name: 'Speechly Expense Tracker', value: ''},
+    {name: 'Reacipe Search App', value: ''}
+  ],
+  resume: 'https://drive.google.com/file/d/1ogs5nz44FeVuwS_ZFAkYVxMhUFi50Kxt/view?usp=sharing',
+  contact: [
+    {name: 'Email', value: 'amrapalipalkar24@gmail.com'},
+    {name: 'mobile', value: '7841834650'},
+    {name: 'LinkedIn', value: 'https://www.linkedin.com/in/amrapali-palkar-030558170/'},
+    {name: 'GitHub', value: 'https://github.com/Amrapalipalkar'},
+    {name: 'Instagram', value: 'https://instagram/imamrapali'}
+  ]
+}
