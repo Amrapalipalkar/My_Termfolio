7e5e0d3e92fcb56fa0ace44bfac0389b8c5027d2
Er. Amrapali Palkar <80786215+Amrapalipalkar@users.noreply.github.com>
2021-10-12 17:18:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -56,11 +56,9 @@ function checkCommand(inputCommand){
     if(command){
         if(command === 'clear'){ commandClear();}
         // else if(command === 'echo') {commandEcho(inputCommand);}
-        else if(command === 'portfolio'){ commandPortfolio();}
         else if(command === 'github'){ commandGithub(command)}
         else if(command === 'start'){ commandHelp();}
         else if(command === 'resume'){ commandResume();}
-//         else if(command === 'portfolio'){ commandPortfolio();}
         else if(commandList.includes(command)){ executeCommand(command);}
         else{
             terminalBody.innerHTML +=  '<br>' + inputCommand + ' is not recognized as a command, Try \"start"\"<br>';
@@ -98,8 +96,7 @@ function commandResume() {
     terminalBody.innerHTML += `<br><a href="resume.pdf"/${data.resume} target="_blank">Resume</a><br>`;
 }
 
- function commandPortfolio() {
-     terminalBody.innerHTML += `<br><a href=""/${data.portfolio} tartget="_blank">Portfolio</a><br>`;
+ 
 
 
 // function commandEcho(inputCommand) {
